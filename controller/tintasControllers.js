055c383deaca06e625334d753d3ec07c56ad3cb6
Debora Passos <deborinhapassos@gmail.com>
2021-10-23 10:23:17 -0300

    Criando controller

--- controller/tintasControllers.js
+++ controller/tintasControllers.js
@@ -1,3 +1,18 @@
+const ERROR = 'Esse formato não é possível';
+
+const tintasController = (req, res) => {
+  const paredes = req.body;
+
+  let areaTotal = 0;
+  for (const parede of paredes) {
+    areaTotal += calcularParede(parede.altura, parede.largura, parede.janela, parede.porta)
+  }
+
+  const quantidadeTinta = calcularQtdTinta(areaTotal);
+
+  return res.status(200).json(quantidadeTinta);
+};
+
 const calcularParede = (altura, largura, janela, porta) => {
   const tamParede = altura * largura;
   const janelas = 2.4 * janela;
@@ -10,3 +25,27 @@ const calcularParede = (altura, largura, janela, porta) => {
   if ((tamParede / 2) < janPor) throw ERROR;
   return tamTotal;
 };
+
+const calcularQtdTinta = (areaTotal) => {
+  const lata05 = 2.5;
+  const lata25 = 12.5;
+  const lata36 = 18;
+  const lata18 = 90;
+
+  if (areaTotal <= lata05) { 
+    return 0.5;
+  }
+  if (areaTotal <= lata25) {
+   return 2.5;
+  
+  }
+  if (areaTotal <= lata36) {
+    return 3.6;
+    
+  }
+  if (areaTotal > lata18) {
+    return 18;
+  }
+}
+
+module.exports = tintasController;
